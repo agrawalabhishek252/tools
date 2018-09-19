51ecaf613ed6f57cd21546179a73bcf08d3ad9b6
agrawalabhishek252 <42511993+agrawalabhishek252@users.noreply.github.com>
2018-09-20 01:23:51 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA0aSjVwZeGCrHP1lf7-93zDqjiTohwooc",
+    authDomain: "tools-list-1c092.firebaseapp.com",
+    databaseURL: "https://tools-list-1c092.firebaseio.com",
+    projectId: "tools-list-1c092",
+    storageBucket: "tools-list-1c092.appspot.com",
+    messagingSenderId: "789316103736"
+  };
+  firebase.initializeApp(config);
---
+++ js/database.js
@@ -0,0 +1,11 @@
+var database = firebase.database();
+var toolslist=database.ref('tools');
+var xyz=toolslist.push(submit);
+	xyz.set({
+
+     
+
+
+
+	});
+
