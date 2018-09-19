1bb8267c07ae8ee8e8fa42cabefa1e17561a8872
agrawalabhishek252 <42511993+agrawalabhishek252@users.noreply.github.com>
2018-09-20 01:02:38 +0530

    Add files via upload

---
+++ tt.js
@@ -0,0 +1,6 @@
+var submit=document.getElementById("submit");
+submit.onclick=function(){
+	var toolname=document.getElementById("name").value;
+	var placekept=document.getElementById("place").value;
+
+}
