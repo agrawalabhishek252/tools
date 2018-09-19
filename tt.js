6ec0a312131ddc43bf255b7865655ab04953a6c3
agrawalabhishek252 <42511993+agrawalabhishek252@users.noreply.github.com>
2018-09-20 01:13:48 +0530

    Delete tt.js

--- tt.js
+++
@@ -1,6 +0,0 @@
-var submit=document.getElementById("submit");
-submit.onclick=function(){
-	var toolname=document.getElementById("name").value;
-	var placekept=document.getElementById("place").value;
-
-}
